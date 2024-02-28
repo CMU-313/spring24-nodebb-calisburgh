4fbc1ba481e64f701875011ec5b86342a71d6cfd
Jiatong Li <102882840+Jiatonglii22@users.noreply.github.com>
2024-02-28 17:26:14 -0500

    changed instances of default plugin to modified plugin

--- test/mocks/databasemock.js
+++ test/mocks/databasemock.js
@@ -253,7 +253,7 @@ async function enableDefaultPlugins() {
     const defaultEnabled = [
         'nodebb-plugin-dbsearch',
         'nodebb-widget-essentials',
-        'nodebb-plugin-composer-default',
+        'nodebb-plugin-composer-default-modified',
     ].concat(testPlugins);
 
     winston.info('[install/enableDefaultPlugins] activating default plugins', defaultEnabled);
