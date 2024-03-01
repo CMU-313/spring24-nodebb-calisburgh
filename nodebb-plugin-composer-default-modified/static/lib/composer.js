356dc0269da39249e972eae13e0a8245bb7127be
Jiatong Li <102882840+Jiatonglii22@users.noreply.github.com>
2024-02-29 19:25:35 -0500

    fixing linter errors

--- nodebb-plugin-composer-default-modified/static/lib/composer.js
+++ nodebb-plugin-composer-default-modified/static/lib/composer.js
@@ -88,15 +88,15 @@ define('composer', [
 
 			if (!isMobile && window.location.pathname.startsWith(config.relative_path + '/compose')) {
 				/*
-				 *	If this conditional is met, we're no longer in mobile/tablet
-				 *	resolution but we've somehow managed to have a mobile
-				 *	composer load, so let's go back to the topic
+				 *If this conditional is met, we're no longer in mobile/tablet
+				 *resolution but we've somehow managed to have a mobile
+				 *composer load, so let's go back to the topic
 				 */
 				history.back();
 			} else if (isMobile && !window.location.pathname.startsWith(config.relative_path + '/compose')) {
 				/*
-				 *	In this case, we're in mobile/tablet resolution but the composer
-				 *	that loaded was a regular composer, so let's fix the address bar
+				 *In this case, we're in mobile/tablet resolution but the composer
+				 *that loaded was a regular composer, so let's fix the address bar
 				 */
 				mobileHistoryAppend();
 			}
@@ -342,7 +342,7 @@ define('composer', [
 
 		submitBtn.on('click', function (e) {
 			e.preventDefault();
-			e.stopPropagation();	// Other click events bring composer back to active state which is undesired on submit
+			e.stopPropagation();// Other click events bring composer back to active state which is undesired on submit
 
 			$(this).attr('disabled', true);
 			post(post_uuid);
@@ -466,8 +466,8 @@ define('composer', [
 			submitOptions: [
 				// Add items using `filter:composer.create`, or just add them to the <ul> in DOM
 				// {
-				// 	action: 'foobar',
-				// 	text: 'Text Label',
+				// action: 'foobar',
+				// text: 'Text Label',
 				// }
 			],
 		};
