1275755b9206a61643c97ce09254db2dfb6d2581
Jiatong Li <102882840+Jiatonglii22@users.noreply.github.com>
2024-02-29 19:28:37 -0500

    fixing linter errors

--- nodebb-plugin-composer-default-modified/static/lib/composer/drafts.js
+++ nodebb-plugin-composer-default-modified/static/lib/composer/drafts.js
@@ -93,8 +93,8 @@ define('composer/drafts', ['api', 'alerts'], function (api, alerts) {
 
 			if (postData.hasOwnProperty('cid') && !postData.save_id.endsWith(':cid:' + postData.cid)) {
 				// A new cid was selected, the save_id needs updating
-				drafts.removeDraft(postData.save_id);	// First, delete the old draft
-				postData.save_id = postData.save_id.replace(/cid:\d+$/, 'cid:' + postData.cid);	// then create a new save_id
+				drafts.removeDraft(postData.save_id);// First, delete the old draft
+				postData.save_id = postData.save_id.replace(/cid:\d+$/, 'cid:' + postData.cid);// then create a new save_id
 			}
 
 			if (raw.length || (title && title.length)) {
